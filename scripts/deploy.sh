7d0d48594c67d07fb8de0fa3904150601f9eb43a
kwang-sub <74045087+kwang-sub@users.noreply.github.com>
2022-10-18 17:04:03 +0900

    배포시 oauth 설정파일 읽도록 설정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -28,5 +28,5 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 nohup java -jar \
-  -Dspring.config.location=classpath:/application.properties,classpath:/application-oauth.properties \
+  -Dspring.config.location=classpath:/application.properties,/home/ec2-user/application/application-oauth.properties \
   $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
