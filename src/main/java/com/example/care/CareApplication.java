0f04d84036663cf19c41dea72f3127805a8dcc29
kwang-sub <74045087+kwang-sub@users.noreply.github.com>
2022-10-18 16:04:57 +0900

    배포 자동화 테스트

--- src/main/java/com/example/care/CareApplication.java
+++ src/main/java/com/example/care/CareApplication.java
@@ -2,6 +2,7 @@ package com.example.care;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -11,7 +12,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class CareApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CareApplication.class, args);
+        SpringApplication application = new SpringApplication(CareApplication.class);
+        application.addListeners(new ApplicationPidFileWriter());
+        application.run(args);
     }
 
 }
